832025616538336c05257646c21fc27de200952c
Kishore Krishna <48233128+kishore-krishna@users.noreply.github.com>
2025-05-20 22:57:41 +0530

    Add files via upload

---
+++ Fun_add.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int Fun_add (int x) {
+    return 5 + x;
+}
+
+int main () {
+    cout << Fun_add(5);
+    return 0;
+}
---
+++ Function1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+void MyCamera () {
+        cout <<"My camera name is Nikon "<< "\n";
+        cout <<"And my friends camera name is Canon" << "\n";
+}
+
+int main (){
+    MyCamera ();
+    return 0;
+}
